c6011dda6a36ee58cd194f040eec6a476da4344b
Mohammed Najjar <najjarmedd@gmail.com>
2021-07-26 16:15:13 +0100

    product id part one

--- client/src/components/pages/ProductId.js
+++ client/src/components/pages/ProductId.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import { Paper } from '@material-ui/core';
 import {useParams} from 'react-router-dom'
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        width: '80%',
+        margin: '1% auto',
+        paddingTop: '2%',
+        paddingBottom: '2%',
+        [theme.breakpoints.down('sm')]: {
+          width: '100%', 
+        },
+      }
+}))
 const ProductId = () => {
+    const classes = useStyles();
     const {id} = useParams();
     return (
-        <div>
-            <h1>product page id number {id}</h1>
-        </div>
+        <Paper className={classes.paper}>
+            <h2 className="text-center">ProductId page</h2>
+            <div className="d-flex justify-content-around mt-5">
+            <div>
+                <h1>product page id number {id}</h1>
+            </div>
+            </div>
+        </Paper>
     )
 }
 
