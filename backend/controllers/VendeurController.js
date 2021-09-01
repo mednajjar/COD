98baaa5068466d0d0e89477da52a302983688aa1
Mohammed Najjar <najjarmedd@gmail.com>
2021-09-01 06:27:27 +0100

    order p1

--- backend/controllers/VendeurController.js
+++ backend/controllers/VendeurController.js
@@ -8,8 +8,8 @@ const Customer = require('../models/Customer');
 const Vendeur = require('../models/Vendeur');
 const Product = require('../models/Product');
 const Validation = require('../models/Validation');
-const {register, validate} = require('../functions/register');
-const {findThing} = require('../functions/functions');
+const { register, validate } = require('../functions/register');
+const { findThing } = require('../functions/functions');
 const { validationRegisterVendeur, validationAddProduct } = require('../validation/validationForm');
 
 /**
@@ -19,8 +19,8 @@ const { validationRegisterVendeur, validationAddProduct } = require('../validati
  * @param send code by email
  */
 
- exports.createVendeur = async (req, res ) => {
-    await register(req, res,validationRegisterVendeur, Admin, Client, Livreur, Vendeur, Customer, Vendeur, Validation)
+exports.createVendeur = async (req, res) => {
+    await register(req, res, validationRegisterVendeur, Admin, Client, Livreur, Vendeur, Customer, Vendeur, Validation)
 }
 
 /**
@@ -28,14 +28,14 @@ const { validationRegisterVendeur, validationAddProduct } = require('../validati
  * @param save vendeur details
  */
 
-exports.validVendeur = async (req, res)=>{
-const { code } = req.body;
+exports.validVendeur = async (req, res) => {
+    const { code } = req.body;
 
     const object = req.cookies.user
     const details = await Validation.findOne({ idUser: object._id });
-   console.log(object)
+    console.log(object)
     try {
-        if (details.key === code){
+        if (details.key === code) {
             const validate = new Vendeur({
                 _id: object._id,
                 nom: object.nom,
@@ -49,17 +49,17 @@ const { code } = req.body;
                 password: object.password
             })
             const saved = await validate.save()
-            if(saved){
+            if (saved) {
                 //create a token
                 const token = jwt.sign({ id: object._id, role: object.role }, process.env.SECRET_TOKEN, { expiresIn: process.env.EXPIRATION_IN })
                 return res.status(201).cookie('auth_token', token, { maxAge: process.env.EXPIRATION_IN, httpOnly: true }).json({ role: object.role, isAuthenticated: true });
                 //  return res.status(201).clearCookie('user').json('Your account created successfuly')
             }
-        }else{
+        } else {
             return res.status(400).json('code doesn\'t match!');
         }
     } catch (error) {
-        if(error) throw Error(error)
+        if (error) throw Error(error)
     }
 }
 
@@ -68,12 +68,12 @@ const { code } = req.body;
  * @param update free pack and listing
  */
 
-exports.freePack = async ( req, res )=>{
+exports.freePack = async (req, res) => {
     // const {id} = req.body;
-    const findUser = await Vendeur.findOne({_id: res.auth._id,});
-    if(findUser){
-        const updatePack = await Vendeur.findOneAndUpdate({_id: findUser._id},{pack: 'free', listing: 40});
-    }else{
+    const findUser = await Vendeur.findOne({ _id: res.auth._id, });
+    if (findUser) {
+        const updatePack = await Vendeur.findOneAndUpdate({ _id: findUser._id }, { pack: 'free', listing: 40 });
+    } else {
         return res.status(400).json('Error request!');
     }
     return res.status(200).json('Vous avez obtenu Free Pack');
@@ -83,14 +83,92 @@ exports.freePack = async ( req, res )=>{
  * @param add product
  */
 
- exports.addProduct = async (req, res)=>{
+exports.addProduct = async (req, res) => {
     const { error } = validationAddProduct(req.body);
     if (error) return res.status(400).json(error.details[0].message);
     let filesArray1 = [];
     let filesArray2 = "";
     // check listing value
-    const listing = await Vendeur.findOne({_id: res.auth._id});
+    const listing = await Vendeur.findOne({ _id: res.auth._id });
 
+    req.files.images.forEach(element => {
+        const file = {
+            fileName: element.originalname,
+            filePath: element.path,
+            fileType: element.mimetype
+        }
+        filesArray1.push(file);
+    });
+    req.files.imgPrincipal.forEach(element => {
+        const file = {
+            fileName: element.originalname,
+            filePath: element.path,
+            fileType: element.mimetype
+        }
+        filesArray2 = file;
+    });
+    try {
+        if (listing && listing.listing <= 0) {
+            return res.status(400).json(`Vous avez atteindre le maximum de listage de ${listing.pack} pack`)
+        } else {
+            const product = new Product({
+                ...req.body,
+                idVendeur: res.auth._id,
+                imgPrincipal: filesArray2.filePath,
+                images: filesArray1
+            })
+            const task = Fawn.Task();
+            const taches = await task.save('Product', product)
+                .update('Vendeur',
+                    { _id: listing._id }, { $inc: { listing: -1 } })
+                .run({ useMongoose: true })
+
+            if (taches) return res.status(201).json('Produit ajouter avec succée');
+
+        }
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+exports.deleteProduct = async (req, res) => {
+    const { id } = req.body;
+    const product = await Product.findOne({ _id: id });
+
+    const task = Fawn.Task();
+    try {
+        if (product) {
+            const taches = await
+                task.update('Vendeur',
+                    { _id: product.idVendeur }, { $inc: { listing: 1 } })
+
+                    .remove('Product', { _id: product._id })
+                    .run({ useMongoose: true })
+
+            if (taches) return res.status(200).json('Produit supprimer avec succée');
+        }
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+exports.getProduct = async (req, res) => {
+    const id = res.auth._id
+    try {
+        const products = await Product.find({ idVendeur: id })
+        if (products) return res.status(200).json(products)
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+exports.updateProduct = async (req, res) => {
+    const { id } = req.params;
+    let filesArray1 = [];
+    let filesArray2 = "";
+    if (req.files) {
         req.files.images.forEach(element => {
             const file = {
                 fileName: element.originalname,
@@ -98,84 +176,6 @@ exports.freePack = async ( req, res )=>{
                 fileType: element.mimetype
             }
             filesArray1.push(file);
-        });
-        req.files.imgPrincipal.forEach(element => {
-            const file = {
-                fileName: element.originalname,
-                filePath: element.path,
-                fileType: element.mimetype
-            }
-            filesArray2 = file;
-        });
-try {
-       if(listing && listing.listing <= 0){
-           return res.status(400).json(`Vous avez atteindre le maximum de listage de ${listing.pack} pack`)
-       }else{
-        const product = new Product({
-            ...req.body,
-            idVendeur: res.auth._id,
-            imgPrincipal: filesArray2.filePath,
-            images: filesArray1
-        })
-        const task = Fawn.Task();
-        const taches = await task.save('Product', product)
-            .update('Vendeur',
-                { _id: listing._id }, {$inc: {listing: -1}})
-            .run({ useMongoose: true })
-
-        if(taches) return res.status(201).json('Produit ajouter avec succée');
-
-       }
-        
-    } catch (error) {
-        return res.status(400).json(error.message);
-    }
- }
-
- exports.deleteProduct = async (req, res)=>{
-     const {id} = req.body;
-     const product = await Product.findOne({_id: id});
-
-     const task = Fawn.Task();
-     try {
-        if(product){
-            const taches = await 
-            task.update('Vendeur',
-                { _id: product.idVendeur }, {$inc: {listing: 1}})
-            
-            .remove('Product', {_id: product._id})
-            .run({ useMongoose: true })
-
-        if(taches) return res.status(200).json('Produit supprimer avec succée');
-        } 
-     } catch (error) {
-         return res.status(400).json(error.message);
-     }
- }
-
- exports.getProduct = async (req, res)=>{
-    const id = res.auth._id
-    try {
-        const products = await Product.find({idVendeur: id})
-        if(products) return res.status(200).json(products)
-        
-    } catch (error) {
-    return res.status(400).json(error.message);
-    }
- }
-
- exports.updateProduct = async (req, res)=>{
-     const {id} = req.params;
-    let filesArray1 = [];
-    let filesArray2 = "";
-    if(req.files){ 
-        req.files.images.forEach(element => {
-        const file = {
-            fileName: element.originalname,
-            filePath: element.path,
-            fileType: element.mimetype
-        }
-        filesArray1.push(file);
         });
         req.files.imgPrincipal.forEach(element => {
             const file = {
@@ -198,5 +198,19 @@ try {
         throw Error(error)
     }
 
- 
- }
+
+}
+
+/**
+* @param get categories
+*/
+
+exports.getCategories = async (req, res) => {
+    try {
+        const category = await Category.find();
+        if (category) return res.status(200).json(category)
+    } catch (error) {
+        if (error) return res.status(400).json(error.message)
+    }
+
+}
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const routes = express.Router();
-const { 
-    createClient, 
-    validClient 
-    } = require('../controllers/ClientController');
-const { 
-    createVendeur, 
-    validVendeur, 
-    freePack, 
+const {
+    createClient,
+    validClient
+} = require('../controllers/ClientController');
+const {
+    createVendeur,
+    validVendeur,
+    freePack,
     addProduct,
     deleteProduct,
-    getProduct, 
+    getProduct,
     updateProduct,
-    } = require('../controllers/VendeurController');
-const {verifToken} = require('../middlewares/verifyToken');
+} = require('../controllers/VendeurController');
+const { getCategories } = require('../controllers/adminController')
+const { verifToken } = require('../middlewares/verifyToken');
 const Vendeur = require('../models/Vendeur');
 const multer = require('../middlewares/multer')
 
@@ -29,21 +30,22 @@ routes.post('/validClient', validClient);
  * @param vendeur routes
  */
 
- routes.post('/createVendeur', createVendeur);
- routes.post('/validVendeur', validVendeur);
- routes.put('/freePack', verifToken('vendeur', Vendeur), freePack);
- routes.post('/addProduct', verifToken('vendeur', Vendeur), multer.fields([{ name: 'images', maxCount: 4 }, { name: 'imgPrincipal', maxCount: 1 }]) , addProduct);
- routes.delete('/deleteProduct', verifToken('vendeur', Vendeur), deleteProduct);
- routes.get('/myProducts', verifToken('vendeur', Vendeur), getProduct);
- routes.put('/updateProduct/:id', verifToken('vendeur', Vendeur), updateProduct)
+routes.post('/createVendeur', createVendeur);
+routes.post('/validVendeur', validVendeur);
+routes.put('/freePack', verifToken('vendeur', Vendeur), freePack);
+routes.post('/addProduct', verifToken('vendeur', Vendeur), multer.fields([{ name: 'images', maxCount: 4 }, { name: 'imgPrincipal', maxCount: 1 }]), addProduct);
+routes.delete('/deleteProduct', verifToken('vendeur', Vendeur), deleteProduct);
+routes.get('/myProducts', verifToken('vendeur', Vendeur), getProduct);
+routes.put('/updateProduct/:id', verifToken('vendeur', Vendeur), updateProduct);
+routes.get('/getCategory', verifToken('vendeur', Vendeur), getCategories)
 
- /**
-  * @param customer routes
-  */
+/**
+ * @param customer routes
+ */
 
 
-  /**
-   * @param livreur routes
-   */
+/**
+ * @param livreur routes
+ */
 
 module.exports = routes;
